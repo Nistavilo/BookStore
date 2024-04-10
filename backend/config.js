13f73ebdfa04e365e84b692a5b2afe974aa7f32d
nistavilo <emirhankorkmaz275@gmail.com>
2024-04-10 23:37:06 +0300

    BookStore

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 3333 ;
+
+export const mongoDBURL =
+'mongodb+srv://<username>:<password>@cluster0.q8hfex2.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
